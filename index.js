0dbc2ef42550b424948c8dcd93b31960c9970cb8
Veteran <pradumnagarg@hotmail.com>
2020-05-01 23:47:52 +0530

    node module http

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require('fs');
+const path = require('path');
+
+const hostname = "localhost";
+const Port = 3000;
+
+const server = http.createServer((req,res)=>{
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+    if(req.method == 'GET') {
+        var fileUrl;
+        if(req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if(!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end("File not found :" + fileUrl);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            }); 
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("This file is not supported : " + fileUrl);
+        }
+    }
+    else { 
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('This method is not supported : ' + req.method);   
+    }
+})
+
+server.listen(Port, hostname, ()=>{
+    console.log(`Server is running at http://${hostname}:${Port}`);
+})
